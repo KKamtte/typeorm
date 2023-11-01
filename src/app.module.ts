9c54d91c38ae1c95d00e96c91dac2accdfb25feb
KKamtte <swye199@gmail.com>
2023-11-02 00:35:11 +0900

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModel } from './entity/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([UserModel]),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: 5433,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'typeormstudy',
+      entities: [UserModel],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
