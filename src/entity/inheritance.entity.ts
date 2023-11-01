a73938ba2ee693a144016fdcebe489f0379377d4
KKamtte <swye199@gmail.com>
2023-11-02 01:27:59 +0900

    table inheritance

---
+++ src/entity/inheritance.entity.ts
@@ -0,0 +1,65 @@
+import {
+  ChildEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  TableInheritance,
+  UpdateDateColumn,
+} from 'typeorm';
+
+// 상속 받은 클래스들이 각각 개별의 테이블을 생성
+export class BaseModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
+
+@Entity()
+export class BookModel extends BaseModel {
+  @Column()
+  name: string;
+}
+
+@Entity()
+export class CarModel extends BaseModel {
+  @Column()
+  brand: string;
+}
+
+// Single Table Inheritance
+// 하나의 테이블로 모든 엔티티를 관리
+@Entity()
+@TableInheritance({
+  column: {
+    name: 'type', // 하나의 테이블 안에서 computer 모델인지 airplane 모델인지 구분하는 칼럼
+    type: 'varchar',
+  },
+})
+export class SingleBaseModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
+
+@ChildEntity()
+export class ComputerModel extends SingleBaseModel {
+  @Column()
+  brand: string;
+}
+
+@ChildEntity()
+export class AirplaneModel extends SingleBaseModel {
+  @Column()
+  country: string;
+}
